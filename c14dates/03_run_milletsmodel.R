77163227eedfd09a1fd6af82236923e6edbc67a9
E. Crema <erc62@troughton.arch.private.cam.ac.uk>
2025-01-21 12:49:33 +0000

    Updated millet

--- c14dates/03_run_milletsmodel.R
+++ c14dates/03_run_milletsmodel.R
@@ -9,15 +9,16 @@ load(here('c14dates','02_millets_run.RData'))
 ncores  <-  4
 cl <- makeCluster(ncores)
 seeds <- c(12,34,56,78)
-niter  <- 6000000
-nburnin  <- 3000000
-thin  <- 300
+niter  <- 2000000
+nburnin  <- 1000000
+thin  <- 100
 
 # Run Model
 out  <-  parLapply(cl = cl, X = seeds, fun = runscript, d = dat.millets, constants = constants.millets, inits = inits.millets, niter = niter, nburnin = nburnin,thin = thin)
 stop(cl)
 
 rhats.millets <- gelman.diag(out,multivariate=T)
+rhats.millets[[1]][which(rhats.millets[[1]][,1]>1.01),]
 ess.millets <- effectiveSize(out)
 post.millets <- do.call(rbind.data.frame,out)
 post.millets.theta <- post.millets[,grepl('theta',colnames(post.millets))]
